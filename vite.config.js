b21eb47dc49a84db7835552c3a0309513606ffdf
Youssef Bourmaya <youssefbourmaya25@gmail.com>
2026-06-23 21:03:58 +0100

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import path from 'node:path';
 
 export default defineConfig({
+  base: '/LocationVoiture/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
